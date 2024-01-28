4af2248a0bd06a351028fa0b6ddfcf3fb5dfbc63
ChengZhouCR <78072843+chengzhouCR@users.noreply.github.com>
2024-01-28 20:05:17 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-      '<script src=/"https://sdk.jinrishici.com/v2/browser/jinrishici.js/"charset=/"utf-8/"></script><div id=/"poem_sentence/"></div><div id=/"poem_info/"></div><script type=/"text/javascript/">jinrishici.load(function(result){var sentence=document.querySelector(/"#poem_sentence/")var info=document.querySelector(/"#poem_info/")sentence.innerHTML=result.data.content info.innerHTML='【'+result.data.origin.dynasty+'】'+result.data.origin.author+'《'+result.data.origin.title+'》'});</script>',
+      '<span id=/"jinrishici-sentence/">正在加载今日诗词....</span><script src=/"https://sdk.jinrishici.com/v2/browser/jinrishici.js/"charset=/"utf-8/"></script>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
