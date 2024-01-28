e4fd503f8ae096005c687859ee925dd3be72be76
ChengZhouCR <78072843+chengzhouCR@users.noreply.github.com>
2024-01-28 19:00:03 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    \'<p id="hitokoto_all"><a href="#" id="hitokoto_text">获取诗词中 ... </a></p>
+    '<p id="hitokoto_all"><a href="#" id="hitokoto_text">获取诗词中 ... </a></p>
     <script async <%= theme.pjax.enable === true ? 'data-pjax' : '' %>
         >
         fetch('https://v1.hitokoto.cn/?c=i')
@@ -52,7 +52,7 @@ module.exports = {
         .catch(function (err) {
         console.error(err);
     })
-</script>\',
+</script>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
